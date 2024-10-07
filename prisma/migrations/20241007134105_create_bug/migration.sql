491f8a6dfbf2984f3a07fe7d37033fe8267c343a
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-10-07 10:42:35 -0300

    feat: create Bug model in Prisma schema

---
+++ prisma/migrations/20241007134105_create_bug/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('OPEN', 'IN_PROGRESS', 'CLOSED');
+
+-- CreateTable
+CREATE TABLE "Bug" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'OPEN',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Bug_pkey" PRIMARY KEY ("id")
+);
