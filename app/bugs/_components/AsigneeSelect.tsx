a29bb85e515c346947ff14c6a724eb36d305e28b
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-11-05 07:16:56 -0300

    refactor: migrate from useEffect to useQuery for user fetching

--- app/bugs/_components/AsigneeSelect.tsx
+++ app/bugs/_components/AsigneeSelect.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import React from "react";
 import axios from "axios";
 import { User } from "@prisma/client";
-import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import {
   Select,
   SelectContent,
@@ -12,21 +13,28 @@ import {
 } from "@/app/_components/ui/select";
 
 const AsigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
+
+  if (error) return <p>Error</p>;
+
+  if (isLoading) return <p>Loading...</p>;
+
   return (
     <Select>
       <SelectTrigger className="col-span-3 border border-gray-400">
         <SelectValue placeholder="Select priority" />
       </SelectTrigger>
       <SelectContent className="border border-gray-700">
-        {users.map((user) => (
+        {users?.map((user) => (
           <SelectItem
             key={user.id}
             value={user.id}
